a536f6eddc4e112a0ab8be03abb9a44b4e5bfa86
Vijay Pandian <ananth.vijay@gmail.com>
2025-03-04 13:10:23 -0500

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "6.8.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "vijay-tf-state-gcp"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = "northamerica-northeast2"
+  zone    = "northamerica-northeast2-a"
+}
+
+
+data "google_storage_bucket" "tf_state_bucket" {
+  name = "vijay-tf-state-gcp"
+}
+
+output "bucket_name" {
+  value = data.google_storage_bucket.tf_state_bucket.name
+}
